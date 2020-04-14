2529230e489d4304a72e62e114ed4f4a31725685
Bianca A. Hernandez <bhernan2@gmail.com>
2020-04-14 17:16:10 -0500

    set up database and flask

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, jsonify
 import numpy as numpy
+import datetime as dt
 
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+from flask import Flask, jsonify
 
+#set up database
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement 
+Station = Base.classes.station 
+
+#set up flask
+app = Flask(_name_)
+
+#set up flask routes: /, /api/v1.0/precipitation, /api/v1.0/stations, /api/v1.0/tobs, /api/v1.0/<start> and /api/v1.0/<start>/<end>
+  
